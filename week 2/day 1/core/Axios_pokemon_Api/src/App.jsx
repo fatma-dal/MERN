3b789f237ccace8c46abe1e93a964d4ae3aa13db
fatma-dal <fatmadalhoumi123@gmail.com>
2025-02-17 17:28:04 +0100

    fetching data using axios

---
+++ week 2/day 1/core/Axios_pokemon_Api/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState , useEffect} from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+function App() {
+  const [pokemons,setPokemons]=useState([])
+
+  const dataWithAxios= async()=>{
+    await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=150')
+    .then(response=>{
+      console.log(response.data.results)
+      
+      const pokemonz=response.data.results
+      
+      setPokemons(pokemonz)
+      
+  
+  })
+  }
+    useEffect(()=>{
+      dataWithAxios()
+    }, []); 
+  
+  
+  return (
+    <>
+    <div className='container col-5 '>
+      <h1 className='text-center'>Pokemon API</h1>
+      <ul className='list-group' >
+        {pokemons.map((pk,index)=> {
+          return(
+            <li className='list-group-item list-group-item-info text-center mt-1 ' key={index}  > <strong>Pokemon Number {index+1}:</strong> {pk.name}</li>  )
+          })}
+      </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
